71e0befbf6df324b44967cfb5f1854554998e7ee
Hyeonje09 <jhj37299455@gmail.com>
2023-07-18 14:47:18 +0900

    모델 훈련(에러투성이)

--- model.py
+++ model.py
@@ -74,6 +74,10 @@ for folder in folders:
 # plt.xticks(class_num, rotation='vertical')
 # plt.show()
 
+learning_rate = 0.001
+batch_size = 100
+training_epochs = 15
+
 # dataset 정의
 train_img = datasets.ImageFolder(root='./archive/Train',
                                      transform=transforms.Compose([
@@ -88,12 +92,12 @@ test_img = datasets.ImageFolder(root='./archive/Test',
                                     )
 
 train_loader = torch.utils.data.DataLoader(train_img,
-                                           batch_size=50,
+                                           batch_size=batch_size,
                                            shuffle=True,
                                            num_workers=8)
 
 test_loader = torch.utils.data.DataLoader(test_img,
-                                          batch_size=50,
+                                          batch_size=batch_size,
                                           shuffle=False,
                                           num_workers=8)
 
@@ -102,10 +106,11 @@ class CNN(torch.nn.Module):
 
     def __init__(self):
         super(CNN,self).__init__()
-        
+        self.keep_prob = 0.5
+
         #첫 번쨰 층
         self.layer1 = torch.nn.Sequential(
-            torch.nn.Conv2d(3, 32, kernel_size=3, input_shape=(img_height,img_width,3)),
+            torch.nn.Conv2d(3, 32, kernel_size=3),
             torch.nn.ReLU(),
             torch.nn.MaxPool2d(kernel_size=2, stride=2),
             torch.nn.Dropout2d(0.25)    
@@ -120,7 +125,49 @@ class CNN(torch.nn.Module):
             )
         
         #세 번째 층
-        self.layer3 = torch.nn.Sequential(
-            torch.nn.Conv2d(64, Kernel_size=3),
-            torch.nn.ReLU()
-        )
+        # self.layer3 = torch.nn.Sequential(
+        #     torch.nn.Conv2d(64, 128, Kernel_size=3),
+        #     torch.nn.ReLU()
+        # )
+        # TypeError: Conv2d.__init__() got an unexpected keyword argument 'Kernel_size' 오류..?
+
+        self.fc1 = torch.nn.Linear(4 * 4 * 128, 625)
+        self.fc2 = torch.nn.Linear(625, 10)
+        
+    #전결합층(Fully-Connteced Layer)
+    def forward(self, x):
+        out = self.layer1(x)
+        out = self.layer2(out)
+        #out = self.layer3(out)
+        out = out.view(out.size(0), -1)
+        out = self.fc2(out)
+        return out
+
+model = CNN().to(device)
+
+criterion = torch.nn.CrossEntropyLoss().to(device)
+optimizer = torch.optim.Adam(model.parameters(), lr = learning_rate)
+
+total_batch = len(train_loader)
+print('총 배치의 수 : {}'.format(total_batch))
+
+def train():
+    for epoch in range(training_epochs):
+        avg_cost = 0
+
+        for X, Y in train_loader:
+            X = X.to(device)
+            Y = Y.to(device)
+
+            optimizer.zero_grad()
+            hypothesis = model(X)
+            cost = criterion(hypothesis, Y)
+            cost.backward()
+            optimizer.step()
+
+            avg_cost += cost / total_batch
+        
+        print('[Epoch :  {:>4}] cost = {:>.9}'.format(epoch+1, avg_cost))
+
+if __name__ == '__main__':
+    train()
