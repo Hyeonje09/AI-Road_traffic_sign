8f8561022e2934423064043b81f6a9e560e4b154
Hyeonje09 <jhj37299455@gmail.com>
2023-07-20 20:32:58 +0900

    Test Loader 수정

--- model.py
+++ model.py
@@ -1,13 +1,17 @@
 import os
 from pathlib import Path
+
+import pandas as pd
 from PIL import Image
-import multiprocessing
+import numpy as np
 
 import torch
 from torch import nn, optim
 from torchvision import transforms
-from torch.utils.data import Dataset, DataLoader
+from torch.utils.data import DataLoader, Dataset
 from torchvision.datasets import ImageFolder
+
+import multiprocessing
 
 # CUDA GPU 사용
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
@@ -22,6 +26,7 @@ if device == 'cuda':
 # 파일 경로 지정
 train_path = Path("./archive/Train")
 test_path = Path("./archive/Test")
+csv_path = Path("./archive/Test.csv")
 
 # 이미지 전처리
 img_height = 30
@@ -44,25 +49,31 @@ batch_size = 64
 train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, num_workers=8)
 
 # 커스텀 테스트 데이터셋 클래스 생성
-class CustomTestDataset(Dataset):
-    def __init__(self, root, transform=None):
-        self.root = root
+class CustomDataset(Dataset):
+    def __init__(self, csv_file, root, transform=None):
+        self.data_info = pd.read_csv(csv_file)  # CSV 파일을 읽어서 데이터프레임으로 저장
+        self.root = root  # 이미지 파일들이 위치한 디렉토리 경로
         self.transform = transform
-        self.images = [os.path.join(root, img_path) for img_path in os.listdir(root) if img_path.endswith('.png')]
-        
+
     def __len__(self):
-        return len(self.images)
+        return len(self.data_info)
 
     def __getitem__(self, index):
-        img_path = self.images[index]
+        img_filename = self.data_info.iloc[index, 7]  # 이미지 파일명 가져오기
+        img_path = self.root / img_filename  # 이미지 파일명을 경로에 맞게 가공
+
         image = Image.open(img_path).convert('RGB')
-        
+
         if self.transform is not None:
             image = self.transform(image)
+
+        label = int(self.data_info.iloc[index, 6])  # 해당 이미지의 라벨(ClassId) 가져오기
+
+        # 이미지 파일과 라벨 확인
+        print(f"Image path: {img_path}, Label: {label}")
+
+        return image, label
         
-        return image
-
-
 # 테스트 데이터셋 로드
 test_transform = transforms.Compose([
     transforms.Resize((img_height, img_width)),
@@ -70,21 +81,21 @@ test_transform = transforms.Compose([
     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
 ])
 
-test_dataset = CustomTestDataset(root=test_path, transform=test_transform)
+test_dataset = CustomDataset(root=test_path, csv_file=csv_path, transform=test_transform)
 test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False, num_workers=8)
 
 # CNN 모델 설계
 class CNN(nn.Module):
     def __init__(self):
         super(CNN, self).__init__()
-        
+
         self.layer1 = nn.Sequential(
             nn.Conv2d(3, 32, kernel_size=3),
             nn.ReLU(),
             nn.MaxPool2d(kernel_size=2, stride=2),
             nn.Dropout2d(0.25)
         )
-        
+
         self.layer2 = nn.Sequential(
             nn.Conv2d(32, 64, kernel_size=3),
             nn.ReLU(),
@@ -101,7 +112,6 @@ class CNN(nn.Module):
         out = out.view(out.size(0), -1)
         out = self.fc1(out)
         out = self.fc2(out)
-        out = nn.functional.softmax(out, dim=1)  # softmax 함수 추가
         return out
 
 def train(model, criterion, optimizer, train_loader):
@@ -132,19 +142,22 @@ def test(model, test_loader):
     total = 0
 
     with torch.no_grad():
-        for images in test_loader:
+        for images, labels in test_loader:
             images = images.to(device)
+            labels = labels.to(device)
 
             outputs = model(images)
             _, predicted = torch.max(outputs.data, 1)
 
-            total += len(images)
-            correct += predicted.cpu().numpy().tolist().count(1)  # 모든 이미지를 클래스 1로 예측
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
 
     accuracy = correct / total * 100
-    print(f'Test Accuracy: {accuracy:.2f}%')
+    print('Test Accuracy: {:.2f}%'.format(accuracy))
 
 if __name__ == '__main__':
+    multiprocessing.set_start_method('spawn')
+
     # CNN 모델 초기화
     model = CNN().to(device)
 
@@ -153,7 +166,7 @@ if __name__ == '__main__':
     optimizer = optim.Adam(model.parameters(), lr=0.001)
 
     # 학습 설정
-    num_epochs = 50
+    num_epochs = 30
 
     multiprocessing.freeze_support()
 
